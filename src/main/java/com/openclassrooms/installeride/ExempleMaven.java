99e998b15a053c88b3a01efae6e4a465db2283f2
lucas.vittaz <lucasvittaz.pro@gmail.com>
2023-01-26 11:30:57 +0100

    init

---
+++ src/main/java/com/openclassrooms/installeride/ExempleMaven.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.installeride;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+public class ExempleMaven {
+
+	public static void main(String[] args) {
+		ImagePlus imp = IJ.openImage();
+		imp.show();
+	}
+
+}
